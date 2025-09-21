457b67e6348b55d4f7815ed8f48bfc540ce8c05d
Joe Avelar <43994252+joeavelar@users.noreply.github.com>
2025-09-21 12:30:05 -0400

    Lab 111 Understanding & Configuring View Encapsulation

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.scss'
+  styleUrl: './control.component.scss',
+  encapsulation: ViewEncapsulation.None // allows us to use SCSS nested classes like .control input, and .control textarea. This is not normally used.
 })
 export class ControlComponent {
   lable = input.required<string>();
