6c2c52c33cb37633d92db772351e86290f35dd6f
Joe Avelar <43994252+joeavelar@users.noreply.github.com>
2025-09-21 15:27:37 -0400

    Lab 114 Interacting with Host Elements from Inside Components.

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,7 +6,11 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.scss',
-  encapsulation: ViewEncapsulation.None // allows us to use SCSS nested classes like .control input, and .control textarea. This is not normally used.
+  encapsulation: ViewEncapsulation.None, // allows us to use SCSS nested classes like .control input, and .control textarea. This is not normally used.
+  host: {
+    // This defined once, but the class is added to 'app-contorl' whereever it's being used. 
+    class: 'control'
+  },
 })
 export class ControlComponent {
   lable = input.required<string>();
