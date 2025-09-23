c51d880e44930a46970e3756222654bf16be0636
Joe Avelar <43994252+joeavelar@users.noreply.github.com>
2025-09-22 21:02:39 -0400

    Lab 121 Maniplulating State & Using Literal Values.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { DashboardItemComponent } from "../dashboard-item/dashboard-item.component";
+
 
 @Component({
   selector: 'app-server-status',
@@ -9,6 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.scss'
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  // When specific string values are used/needed, use the TypeScript feature called "Literal Types"
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online'; 
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random(); // 0 to 0.99999
+
+      if (rnd < 0.5 ) {
+        this.currentStatus = 'online';
+        // console.log('online');
+      } else if (rnd < 0.9 ) {
+        this.currentStatus = 'offline';
+        // console.log('offline');
+      } else {
+        this.currentStatus = 'unknown';
+        // console.log('unknwon');
+      }
+    }, 5000); 
+  }
 
 }
