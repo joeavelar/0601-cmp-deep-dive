539c219ed6fa9c406dc84acf8bc5e0ad4c4f8ffd
Joe Avelar <43994252+joeavelar@users.noreply.github.com>
2025-09-22 20:14:08 -0400

    Lab 116 Interacting with Host elements via @HostListener & @HostBinding.

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,10 +9,24 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None, // allows us to use SCSS nested classes like .control input, and .control textarea. This is not normally used.
   host: {
     // This defined once, but the class is added to 'app-contorl' whereever it's being used. 
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   },
 })
 export class ControlComponent {
+  // This is similar to using 'host:' within the '@Component()' section
+  // This method of using HostBinding() is discouraged. Just here as an example only. Used for backward compatability. 
+  // @HostBinding('class') className = 'control';
+
+
+  // @HostListener('click')   onClick() {
+  //   console.log('Clicked!');
+  // };
+
   lable = input.required<string>();
+
+  onClick() {
+    console.log('Clicked!');
+  }
 
 }
